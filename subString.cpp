7955c6cd12e6eaad8760e712c775195918eac0bd
nayan32006 <nayang389@gmail.com>
2026-04-01 11:22:50 +0530

    done substring

---
+++ subString.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+
+    std:: string string("this is a string");
+    std:: string sub_string(string.substr(4, 6));
+
+    return 0;
+}
